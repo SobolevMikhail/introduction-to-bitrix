cdb0a79e443980352ea155d583cb96a1ce4532fd
SobolevMikhail <msobolev.nnov@gmail.com>
2018-01-13 22:06:20 +0300

    Add new template breadcrumb

--- backend/local/templates/first/components/bitrix/breadcrumb/.default/template.php
+++ backend/local/templates/first/components/bitrix/breadcrumb/.default/template.php
@@ -13,44 +13,23 @@ if(empty($arResult))
 
 $strReturn = '';
 
-//we can't use $APPLICATION->SetAdditionalCSS() here because we are inside the buffered function GetNavChain()
-$css = $APPLICATION->GetCSSArray();
-if(!is_array($css) || !in_array("/bitrix/css/main/font-awesome.css", $css))
-{
-	$strReturn .= '<link href="'.CUtil::GetAdditionalFileURL("/bitrix/css/main/font-awesome.css").'" type="text/css" rel="stylesheet" />'."\n";
-}
-
-$strReturn .= '<div class="bx-breadcrumb">';
+$strReturn .= '<div class="klim-head-breadcrumb"><ul>';
 
 $itemSize = count($arResult);
 for($index = 0; $index < $itemSize; $index++)
 {
 	$title = htmlspecialcharsex($arResult[$index]["TITLE"]);
 
-	$nextRef = ($index < $itemSize-2 && $arResult[$index+1]["LINK"] <> ""? ' itemref="bx_breadcrumb_'.($index+1).'"' : '');
-	$child = ($index > 0? ' itemprop="child"' : '');
-	$arrow = ($index > 0? '<i class="fa fa-angle-right"></i>' : '');
-
 	if($arResult[$index]["LINK"] <> "" && $index != $itemSize-1)
 	{
-		$strReturn .= '
-			<div class="bx-breadcrumb-item" id="bx_breadcrumb_'.$index.'" itemscope="" itemtype="http://data-vocabulary.org/Breadcrumb"'.$child.$nextRef.'>
-				'.$arrow.'
-				<a href="'.$arResult[$index]["LINK"].'" title="'.$title.'" itemprop="url">
-					<span itemprop="title">'.$title.'</span>
-				</a>
-			</div>';
+		$strReturn .= '<li class="bc-link"><a href="/">'.$title.'</a></li>';
 	}
 	else
 	{
-		$strReturn .= '
-			<div class="bx-breadcrumb-item">
-				'.$arrow.'
-				<span>'.$title.'</span>
-			</div>';
+		$strReturn .= '<li class="bc-final">'.$title.'</li>';
 	}
 }
 
-$strReturn .= '<div style="clear:both"></div></div>';
+$strReturn .= '</ul></div>';
 
 return $strReturn;
